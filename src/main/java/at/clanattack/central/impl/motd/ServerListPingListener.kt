7c2ec57a9b9738dc3ab383b269f36db5a49b0894
CheeseTastisch <56408700+CheeseTastisch@users.noreply.github.com>
2022-11-29 13:28:06 +0100

    Added server icon

--- src/main/java/at/clanattack/central/impl/motd/ServerListPingListener.kt
+++ src/main/java/at/clanattack/central/impl/motd/ServerListPingListener.kt
@@ -1,5 +1,6 @@
 package at.clanattack.central.impl.motd
 
+import at.clanattack.bootstrap.ICore
 import at.clanattack.central.top.state.gameState
 import at.clanattack.central.top.state.timeToGameStateChange
 import at.clanattack.top.message.getMessage
@@ -8,8 +9,20 @@ import at.clanattack.utility.listener.ListenerTrigger
 import com.destroystokyo.paper.event.server.PaperServerListPingEvent
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.JoinConfiguration
+import org.bukkit.Bukkit
+import org.bukkit.util.CachedServerIcon
+import java.io.File
 
-class ServerListPingListener {
+class ServerListPingListener(core: ICore) {
+
+    private val icon: CachedServerIcon
+
+    init {
+        val file = File(core.dataFolder, "icon.png")
+        if (!file.exists()) throw IllegalStateException("Server icon is not set.")
+
+        icon = Bukkit.loadServerIcon(file)
+    }
 
     @ListenerTrigger(PaperServerListPingEvent::class)
     fun serverListPing(event: PaperServerListPingEvent) {
@@ -27,6 +40,8 @@ class ServerListPingListener {
                 getMessage("central.ping.motd.${gameState.short}.2", *replacements)
             )
         )
+
+        event.serverIcon = this.icon
     }
 
 }
